5b72cc5f20d6adf55bcc5d740ccbe7dfe2235376
TomLev816 <thomaslever508@gmail.com>
2019-11-08 18:19:10 -0500

    setup layout of page

--- frontend/hockey-warehouse-frontend/src/App.js
+++ frontend/hockey-warehouse-frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-top">
+        Welcome to Hockey Compares
+      </div>
+      <div className="App-selection">
+        Please select 2 players
+      </div>
+      <div className="App-compare">
+        Here are comparisons for the 2 players you selected
+      </div>
     </div>
   );
 }
